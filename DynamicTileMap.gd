6af66fffb78003e45b23c678dd5b29af4408cc82
lyapkoandy13 <lyapkoandy13@gmail.com>
2023-09-17 22:00:37 +0300

    wip

--- DynamicTileMap.gd
+++ DynamicTileMap.gd
@@ -1,9 +1,10 @@
 @tool
 extends Node2D
 
-@export var tile_size: float = 50.0 # px
+@export var tile_size: float = 50.0
+@export var border_size: float = 2.0
 @export var size: int = 10
-@export var show: bool = true
+@export var show: bool = true 
 
 var tile_map: Array = [] # null|'apple'|'snake';
 
@@ -12,20 +13,18 @@ func get_random_position():
 	var x = randi() % int(size);
 	return Vector2(x, y)
 
-func calc_tile_position(position: Vector2) -> Vector2:
-	var center_offset = size / 2
-	var offset = center_offset * tile_size
+func calc_tile_position(position: Vector2, offset: float = 0) -> Vector2:
+	var center_offset = size / 2 * tile_size
 
-	var x = position.x * tile_size - offset
-	var y = position.y * tile_size - offset
-	return Vector2(x, y)
+	var x = position.x * tile_size - center_offset
+	var y = position.y * tile_size - center_offset
+	return Vector2(x, y) + Vector2(offset, offset)
 
-func _draw_tile_borders(position: Vector2) -> void:
+func draw_tile_borders(position: Vector2) -> void:
 	draw_rect(
 		Rect2(position, Vector2(tile_size, tile_size)),
 		Color.DIM_GRAY,
-		true,
-		2.0
+		true
 	)
 	draw_rect(
 		Rect2(position, Vector2(tile_size, tile_size)),
@@ -34,11 +33,11 @@ func _draw_tile_borders(position: Vector2) -> void:
 		2.0
 	)
 	
-func _draw_tile_map() -> void:
+func draw_tile_map() -> void:
 	for y in size:
 		for x in size: 
 			var pos = calc_tile_position(Vector2(x, y))
-			_draw_tile_borders(pos)
+			draw_tile_borders(pos)
 	
 
 func _init() -> void:
@@ -52,5 +51,5 @@ func _draw() -> void:
 	if not show:
 		return
 		
-	_draw_tile_map()
+	draw_tile_map()
 	
---
+++ snake/SnakeController.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+@onready var timer: Timer = Timer.new()
+@onready var Snake = get_parent()
+
+
+const BASE_SPEED = 0.5
+
+enum Direction { UP, DOWN, LEFT, RIGHT }
+
+var current_direction: Direction = Direction.RIGHT
+
+func get_opposite_direction(direction: Direction) -> Direction:
+	match direction:
+		Direction.UP:
+			return Direction.DOWN
+		Direction.DOWN:
+			return Direction.UP
+		Direction.LEFT:
+			return Direction.RIGHT
+		Direction.RIGHT:
+			return Direction.LEFT
+		_:
+			return Direction.LEFT 
+
+func set_current_direction():
+	var direction_x = Input.get_axis("move_left", "move_right")
+	var direction_y = Input.get_axis("move_up", "move_down")
+	
+	var new_direction = null
+	if direction_x > 0:
+		new_direction = Direction.RIGHT
+	elif direction_x < 0:
+		new_direction = Direction.LEFT
+	elif direction_y > 0:
+		new_direction = Direction.DOWN
+	elif direction_y < 0:
+		new_direction = Direction.UP
+		
+	if new_direction == null:
+		return
+		
+	# forbid opposite direction
+	if Snake.size() > 1:
+		if get_opposite_direction(current_direction) == new_direction:
+			return
+	
+	current_direction = new_direction
+
+func _on_timer_timeout():
+	print("Timeout")
+	Snake.move()
+
+func _ready():
+	add_child(timer)
+	timer.timeout.connect(_on_timer_timeout)
+	timer.start(BASE_SPEED)
+
+func _input(event):
+	set_current_direction()
+	
+func _exit_tree():
+	timer.queue_free()
